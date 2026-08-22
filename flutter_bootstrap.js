2b057b09f8e846e83df217b5535e1fca0fc677c3
naveengoud-max <naveengoud-max@users.noreply.github.com>
2026-08-22 05:31:52 +0000

    deploy: bf75b66468373d7d6dcbee6b40aa3ef4bc397f3d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2592034885" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3988041030" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
